f5a8c54bbf9aeee0ea3e887d8880427ddbfcf9c3
mladimilance <96388566+mladimilance@users.noreply.github.com>
2022-05-19 15:08:50 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var randomImage1 = "images/dice" + randomNumber1 + ".png";
+var randomImage2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".dice .img1").setAttribute("src", randomImage1);
+document.querySelector(".dice .img2").setAttribute("src", randomImage2);
+
+if (randomImage1 > randomImage2) {
+  document.querySelector("h1").innerText = "🚩Player 1 Wins";
+} else if (randomImage1 < randomImage2) {
+  document.querySelector("h1").innerText = "Player 2 Wins!🚩";
+} else if (randomImage1 === randomImage2) {
+  document.querySelector("h1").innerText = "Draw!";
+}
